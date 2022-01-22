33671ceeaf0a6c7d271b7449241ec23127f262dc
Alex Li <asl11@rice.edu>
2022-01-21 19:10:54 -0600

    initial commit

---
+++ Main/BufferMgr/headers/MyDB_Page.h
@@ -0,0 +1,22 @@
+#ifndef PAGE_H
+#define PAGE_H
+
+#include "MyDB_PageHandle.h"
+#include "MyDB_Page.h"
+
+using namespace std;
+
+class MyDB_Page {
+    
+    int pageId;
+    int pageFreq;
+
+
+public:
+
+
+private:
+
+};
+
+#endif
---
+++ Main/BufferMgr/source/MyDB_Page.cc
@@ -0,0 +1,7 @@
+#ifndef PAGE_C
+#define PAGE_C
+
+#include "MyDB_PageHandle.h"
+#include "MyDB_Page.h"
+
+#endif
